8e855ac37af0344e1f0346bb5822b8ea1555b36f
Charlotte Urquhart <curquhart@uchicago.edu>
2024-11-29 00:15:54 +0000

    save2

--- app/controllers/department_controller.rb
+++ app/controllers/department_controller.rb
@@ -7,14 +7,14 @@ class DepartmentsController < ApplicationController
 
   def show
     the_id = params.fetch("path_id")
-    @department = Department.where({:id => the_id })
+    @departments = Department.where({:id => the_id })
 
     render({ :template => "departments/show" })
   end
 
   def create
-    @department = Department.new
-    @department.name = params.fetch("query_name")
+    @departments = Department.new
+    department.name = params.fetch("query_name")
 
     if @department.valid?
       @department.save
@@ -28,13 +28,13 @@ class DepartmentsController < ApplicationController
     the_id = params.fetch("path_id")
     @department = Department.where({ :id => the_id }).at(0)
 
-    @department.name = params.fetch("query_name")
+    department.name = params.fetch("query_name")
 
     if @department.valid?
       @department.save
-      redirect_to("/departments/#{@department.id}", { :notice => "Department updated successfully."} )
+      redirect_to("/departments/#{department.id}", { :notice => "Department updated successfully."} )
     else
-      redirect_to("/departments/#{@department.id}", { :alert => "Department failed to update successfully." })
+      redirect_to("/departments/#{department.id}", { :alert => "Department failed to update successfully." })
     end
   end
 
